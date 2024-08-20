451d7575e62a92e804e637a986b21c026018a431
loindhe172148 <loindhe172148@fpt.edu.vn>
2024-08-20 16:25:08 +0700

    up

--- script.js
+++ script.js
@@ -2,11 +2,10 @@ function scrollToTop() {
   var element = document.getElementById("slider");
   element.scrollIntoView({ behavior: "smooth" });
 }
-document.getElementById("menu_toggle").addEventListener("click", function () {
-  var nav = document.getElementById("nav");
-  if (nav.style.display === "block") {
-    nav.style.display = "none";
-  } else {
-    nav.style.display = "block";
-  }
+document.querySelector(".menu-toggle").addEventListener("click", function () {
+  const menu = document.querySelector(".menu");
+  menu.style.display =
+    menu.style.display === "none" || menu.style.display === ""
+      ? "flex"
+      : "none";
 });
